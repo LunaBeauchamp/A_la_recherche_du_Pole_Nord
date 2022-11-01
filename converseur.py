a5630eefaf2a76aa3c8c6a26074346d3f934efb2
LunaBeauchamp <116032544+LunaBeauchamp@users.noreply.github.com>
2022-11-01 10:12:42 -0400

    consigne

---
+++ converseur.py
@@ -0,0 +1,16 @@
+#Partie 1:
+
+#Vous travaillez sur un système de géolocation 
+# s'intitulant à la recherche du pôle Nord 
+# utilisant des coordonnées sous la forme degrés minutes secondes (DMS).
+
+#Pour faciliter l'utilisation du système, 
+# on vous demande de créer un court programme permettant de convertir ces données sous la forme de degrés décimaux (DD).
+
+#De plus, considérant que le but de l'application est d'indiquer la distance des usagers du pôle Nord magnétique
+#  en plus d'indiquer leur position, on vous demande d'ajouter à votre programme une fonction permettant de calculer cette distance.
+
+#Versionner votre travail avec GitHub desktop et publié le sur votre profil GitHub Web une fois terminé.
+
+#decimal degrees = degrees + (minutes / 60) + (seconds/3600)
+
