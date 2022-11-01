1fafc64fed38d196be20bc6638004bb2e4bec5bd
LunaBeauchamp <116032544+LunaBeauchamp@users.noreply.github.com>
2022-11-01 11:57:04 -0400

    part 2

--- géolocation.py
+++ géolocation.py
@@ -30,41 +30,58 @@ def pole_user(position):
     LONG_POLE_N = 162.867
 
     lat_user, long_user = position
-
     lat_user = dms_to_dd(lat_user)
     long_user = dms_to_dd(long_user)
+
     return distance_2_points(LAT_POLE_N, LONG_POLE_N, lat_user, long_user)
 
-lat_user = 45, 30, 31.9968
-long_user = 73, 33, 42.0048
+lat_user = 11, 14, 22.3
+long_user = 87, 46, 25.6
 info_user = lat_user, long_user
-#print(pole_user(info_user))
+print(pole_user(info_user))
+
+#En partant de l'exercice du système de géolocation, 
+# modifiez votre code pour que les positions en DMS incluent la direction cardinale(N, S, E, W ou O) 
+# et retourne une position en DD pouvant être négative. 
+# Modifiez ensuite votre code pour permettre à un utilisateur de manuellement rentrer sa position.
 
 def dms_to_dd(dms):
-    degres, minutes, secondes = dms
-    return degres + minutes/60 + secondes/3600
+    degrees, minutes, secondes, position = dms
+    dd = degrees + (minutes / 60) + (secondes / 3600)
+    if position == 'S' or position == 'W':
+        dd = dd * -1
+    return dd
 
-def distance_2_point(position1, position2):
-    x1, y1 = position1
-    x2, y2 = position2
-    return ((x2-x1) ** 2 + (y2-y1) ** 2) ** 0.5
-    #math.sqrt()
+def distance_2_points(x1, y1, x2, y2):
+    b = x2 - x1
+    c = y2 - y1
+    distance = (b ** 2 + c**2) ** 0.5
+    return distance
 
-def distance_pole_nord(position):
-    POLENORD_LAT = 86.494
-    POLENORD_LONG = 162.867
-    POLE_NORD = POLENORD_LAT, POLENORD_LONG
+def pole_user():
+    LAT_POLE_N = 86.494
+    LONG_POLE_N = -162.867
 
-    lat, long = position
-    lat_dd = dms_to_dd(lat)
-    long_dd = dms_to_dd(long)
-    position_dd = lat_dd, long_dd
+    lat_degrees = float(input('Entrer vos degrees de latitude (11): '))
+    lat_minutes = float(input('Entrer vos minutes de latitude (14): '))
+    lat_secondes = float(input('Entrer vos secondes de latitude (22.3): '))
+    lat_ns = str(input('Entrer votre indication nord/sud (N/S): '))
+    lat_user = lat_degrees, lat_minutes, lat_secondes, lat_ns
+    
+    long_degrees = float(input('Entrer vos degrees de longitude (87): '))
+    long_minutes = float(input('Entrer vos minutes de longitude (46): '))
+    long_secondes = float(input('Entrer vos secondes de longitude (25.6): '))
+    long_ew = str(input('Entrer votre indication est/west (E/W): '))
+    long_user = long_degrees, long_minutes, long_secondes, long_ew
+    
+    lat_user = dms_to_dd(lat_user)
+    long_user = dms_to_dd(long_user)
 
-    return distance_2_point(position_dd, POLE_NORD)
+    return distance_2_points(LAT_POLE_N, LONG_POLE_N, lat_user, long_user), lat_user, long_user
 
-longitude = 45, 30, 31.9968
-latitude = 73, 33, 42.0048
-position = longitude, latitude
 
-dist = distance_pole_nord(position)
-print(dist)
+lat = 11, 14, 22.3, 'S' 
+long = 87, 46, 25.6, 'W'
+print(pole_user())
+#lat_dd = -11.239518
+#long_dd = -87.773767
